500bccc8f93dd0630bdf7550afd81f4446fc4218
Soumya Patil <getme1104@gmail.com>
2020-04-23 16:45:18 -0700

    8/8 Queries done

--- EmployeeSQL/Employee_Query.sql
+++ EmployeeSQL/Employee_Query.sql
@@ -7,13 +7,13 @@ e.emp_no = s.emp_no;
 
 --2 . List employees who were hired in 1986.
 
-SELECT first_name, last_name FROM employees
+SELECT first_name, last_name ,hire_date FROM employees
 WHERE hire_date < '1987-01-01' and hire_date > '1986-01-01';
 
 
 -- 3.List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
 
-SELECT dm.dept_no, d.dept_name , dm.emp_no, e.last_name,e.first_name,dm.from_date, dm.to_date 
+SELECT dm.dept_no, d.dept_name , dm.emp_no, e.last_name,e.first_name,dm.from_date AS start_date, dm.to_date AS end_date
 FROM (
 	(dept_manager AS dm
 	 INNER JOIN employees AS e ON dm.emp_no = e.emp_no)
@@ -37,7 +37,7 @@ WHERE e.first_name = 'Hercules'
 AND e.last_name LIKE 'B%';
 
 
--- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+-- 6. List all employees in the Sales department, including their employee number,last name, first name, and department name.
 
 SELECT de.emp_no, e.last_name,e.first_name, d.dept_name
 FROM (
@@ -47,7 +47,7 @@ FROM (
      )
 WHERE d.dept_name = 'Sales' ; 
 
--- 7.List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+-- 7.List all employees in the Sales and Development departments,including their employee number, last name, first name, and department name.
 
 SELECT de.emp_no, e.last_name,e.first_name, d.dept_name
 FROM (
@@ -63,4 +63,4 @@ OR d.dept_name = 'Sales' ;
 SELECT last_name , COUNT(emp_no) AS counts_of_last_names
 FROM employees
 GROUP BY last_name
-ORDER BY last_name 
+ORDER BY last_name; 
